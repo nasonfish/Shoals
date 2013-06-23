ed172ef825bc15b0464bc67f639a7e128b33b101
Kyle Lockwood <puffrfish@puffrfish.com>
2013-06-23 17:01:05 -0400

    Changed some of the home page wording

--- modules/Index/templates_app/welcome.tpl.php
+++ modules/Index/templates_app/welcome.tpl.php
@@ -28,14 +28,16 @@
         <hr/>
         <div id="questions" class="alert alert-info">
             <h3>So what is Shoals?</h3>
-            <p><strong>Shoals</strong> is a new web app in development by nasonfish, part of the pufferfi.sh umbrella.</p>
+            <p><strong>Shoals</strong> is a new web app in development by nasonfish, part of the pufferfi.sh community of developers.</p>
             <p>Shoals, the app, allows you to make your own community forum/page, also called a "Shoal", with just a few simple clicks.</p>
             <p>You can customize your shoal using "plugins" that you can add, which will range from text boxes that you can edit to programs that check if a Minecraft server is up, and more.</p>
-            <p>Hopefully, your shoal will allow your community to be more linked together and provide a place for people to be active in to keep your community closer.</p>
+            <p>Your shoal will allow your community to be more linked together and provide a place for your "shoalmates" to communicate to keep your community closer.</p>
             <hr/>
             <h3>What is a 'shoal' anyway?</h3>
-            <p>A shoal is like a group of fish that stick together, but do not move together like a school does. Kind of like a fish community. <strong>quote from puffrfish on IRC.</strong></p>
-            <p>puffrfish and I really like fish, and in the past, we've created some fish-themed stuff, so naming this project "Shoals" really fits in.</p>
+            <p>A shoal is a group of fish that stick together, but do not move together like a school does. Kind of like a fish community. <strong>quote from puffrfish on IRC.</strong></p>
+			<p>Shoaling fish are not necessarily of the same species or move in a coordinated manner, but they are a tight-knit community that sticks together for a common goal.
+            <p>puffrfish and I really like fish, and in the past, we've created some fish-themed stuff, so naming this project "Shoals" really suits us well.</p>
+			<p>We hope Shoals can provide you with the same strong connection between your community members, and will be keeping community needs in mind throughout the development of the project.
             <hr/>
             <h3>How do I contact you guys?</h3>
             <p>You can find us on the <a href="http://esper.net/">EsperNet Internet Relay Chat network</a> in the channel <strong>#Shoals</strong>. <a href="http://webchat.esper.net/?channels=Shoals">Here's an easy link for accessing the channel.</a></p>
